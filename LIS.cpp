7cc42297657c882f79712fe95fa3ac9839e10566
Vatsal Sharma <vatsalsharma376@gmail.com>
2016-07-22 21:59:41 +0530

    LIS nlogn version
    Time Complexity - O(n lg n) ~ worst
    Space Complexity - O(n)

---
+++ LIS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	multiset <int> s;
+	multiset<int>:: iterator it;
+	int n;
+	cin>>n;
+	int x;
+	for(int i=0;i<n;i++)
+	{
+		cin>>x;
+		s.insert(x);
+		it = s.find(x);
+		it++;
+		if(it!=s.end())
+			s.erase(it);
+		
+	}
+	cout<<s.size();
+	return 0;
+}
